3dc8ffb1359fdbd07d355401033b0e88936b9f67
Savienkov Maksym <froggt25@gmail.com>
2021-11-10 13:06:14 +0200

    Добавьте файлы проекта.

---
+++ Interfaces/MyComplex.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    class MyComplex : IMyNumber<MyComplex>
+    {
+        private double re;
+        private double im;
+
+        public MyComplex(double re, double im)
+        {
+            this.re = re;
+            this.im = im;
+        }
+
+        public MyComplex Add(MyComplex that)
+        {
+            return new MyComplex(re + that.re, im + that.im);
+        }
+
+        public MyComplex Divide(MyComplex that)
+        {
+            double nom_re = re * that.re + im * that.im;
+            double denom = Math.Pow(that.re, 2) + Math.Pow(that.im, 2);
+            double nom_im = im * that.re - that.im * re;
+
+            return new MyComplex(nom_re / denom, nom_im / denom);
+        }
+
+        public MyComplex Multiply(MyComplex that)
+        {
+            return new MyComplex(re * that.re - im * that.im, im * that.re + that.im * re);
+        }
+
+        public MyComplex Subtract(MyComplex that)
+        {
+            return new MyComplex(re - that.re, im - that.im);
+        }
+
+        public override string ToString()
+        {
+            if (im < 0)
+                return re.ToString() + im.ToString() + "i";
+            else
+                return re.ToString() + "+" + im.ToString() + "i";
+        }
+    }
+}
---
+++ Interfaces/MyFrac.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    class MyFrac : IMyNumber<MyFrac>, IComparable<MyFrac>
+    {
+        private BigInteger nom;
+        private BigInteger denom;
+
+        public MyFrac(int nom, int denom)
+        {
+            if (denom == 0)
+            {
+                throw new DivideByZeroException();
+            }
+            this.nom = nom;
+            this.denom = denom;
+        }
+
+        public MyFrac(BigInteger nom, BigInteger denom)
+        {
+            if (denom == 0)
+            {
+                throw new DivideByZeroException();
+            }
+            this.nom = nom;
+            this.denom = denom;
+        }
+
+        public MyFrac Add(MyFrac that)
+        {
+            return new MyFrac(nom * that.denom + that.nom * denom, denom * that.denom);
+        }
+
+        public MyFrac Divide(MyFrac that)
+        {
+            if (that.nom == 0)
+            {
+                throw new DivideByZeroException();
+            }
+            return new MyFrac(nom * that.denom, denom * that.nom);
+        }
+
+        public MyFrac Multiply(MyFrac that)
+        {
+            return new MyFrac(nom * that.nom, denom * that.denom);
+        }
+
+        public MyFrac Subtract(MyFrac that)
+        {
+            return new MyFrac(nom * that.denom - that.nom * denom, denom * that.denom);
+        }
+
+        private void Normalise()
+        {
+            BigInteger n = nom, d = denom;
+            while (n % 2 == 0 && d % 2 == 0)
+            {
+                n = n / 2;
+                d = d / 2;
+            }
+            if (d / n % 1 == 0)
+            {
+                d = d / n;
+                n = 1;
+            }
+            nom = n;
+            denom = d;
+        }
+
+        public override string ToString()
+        {
+            Normalise();
+            return nom.ToString() + "/" + denom.ToString();
+        }
+
+        public int CompareTo(MyFrac other)
+        {
+            return (nom / denom).CompareTo(other.nom / other.denom);
+        }
+    }
+}
